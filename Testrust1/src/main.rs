76e98aee528844ef3afd20903c5c96c5da1a48dc
XW-abap <yongxuanyisheng@gmail.com>
2025-05-06 22:10:57 +0800

    tdo
    tdo

--- Testrust1/src/main.rs
+++ Testrust1/src/main.rs
@@ -16,6 +16,17 @@ fn main() {
     // 下面语句输出 " 1" ，5 个空格后面连着 1 。
     println!("{number:>width$}", number=1, width=6);
 
-
+    // 你可以在数字左边补 0 。下面语句输出 "000001" 。
+    println!("{number:>0width$}", number=1, width=6);
+    // println! 会检查使用到的参数数量是否正确。
+    println!("My name is {0}, {1} {0}", "Bond", "James);");
+    // 改正 ^ 补上漏掉的参数："James"
+    // 创建一个包含单个 `i32` 的结构体（structure ） 。命名为 `Structure` 。
+    #[allow(dead_code)]
+    struct Structure(i32);
+    // 但是像结构体这样的自定义类型需要更复杂的方式来处理。
+    // 下面语句无法运行。
+    // println!("This struct `{}` won't print...", Structure(3));
+    // 改正 ^ 注释掉此行。
 }
 
