8dd4b0e000ed5a99f5fa4bb29d6166dcd9fffde1
xiaoyong <yongxuanyisheng@gmail.com>
2026-05-19 14:24:28 +0800

    dd
    dd

--- comparison/src/main.rs
+++ comparison/src/main.rs
@@ -1,6 +1,9 @@
 use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
+// 引入必要的标准库模块来控制时间和线程休眠
+use std::thread;
+use std::time::Duration;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -16,12 +19,17 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
+        println!("You guessed: {guess}");
         match guess.cmp(&secret_number) 
         {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
+                // 增加一句提示，让用户知道正在等待
+                println!("程序将在 3 秒后自动退出...");
+                // 让当前线程休眠 3 秒
+                thread::sleep(Duration::from_secs(3));
                 break;
             }
         }
