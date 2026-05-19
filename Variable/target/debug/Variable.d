72d6b919de9dad3559869e6930934a0477551a8f
XW-abap <yongxuanyisheng@gmail.com>
2026-05-19 09:50:19 +0000

    dd

---
+++ Variable/target/debug/Variable.d
@@ -0,0 +1,1 @@
+/workspaces/Rustproject/Variable/target/debug/Variable: /workspaces/Rustproject/Variable/src/main.rs
---
+++ Variable/target/debug/deps/Variable-3d187a6723d2affc.d
@@ -0,0 +1,5 @@
+/workspaces/Rustproject/Variable/target/debug/deps/Variable-3d187a6723d2affc.d: src/main.rs
+
+/workspaces/Rustproject/Variable/target/debug/deps/Variable-3d187a6723d2affc: src/main.rs
+
+src/main.rs:
